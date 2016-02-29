a31a8769fb02c6b026698ae80f629e9c20c203b0
kschap <kmschapm@oakland.edu>
2016-02-29 02:36:45 -0500

    Added "BTC NOW" functionality.

---
+++ codeparser.php
@@ -0,0 +1,14 @@
+<?php
+$code = $_POST['code'];
+
+if (strtoupper($code) == "BTC NOW") {
+  $request = 'https://blockchain.info/ticker';
+  $response = file_get_contents($request);
+  $decode = json_decode($response, true);
+
+  $price = $decode['USD'][]
+  echo "<h1>" . $code . "</h1></br>";
+  echo "<h2>Bid: " . $decode['USD']['buy'] . "</h2></br>";
+  echo "<h2>Ask: " . $decode['USD']['sell'] . "</h2></br>";
+}
+?>
