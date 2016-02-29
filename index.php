f210f8100c282c24c290a4bced6f9cdb60952788
kschap <kmschapm@oakland.edu>
2016-02-29 02:21:57 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Finance Watcher</title>
+</head>
+
+<body>
+<form action="codeparser.php" method="get">
+  Enter a code:
+  <input type="text" name "code">
+  <input type="submit" value="GO">
+</form>
+</body>
+</html>
