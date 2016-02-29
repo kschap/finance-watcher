4428b21e4342d5d4ccc656522ea3a54fc8dc21a5
kschap <kmschapm@oakland.edu>
2016-02-29 02:41:59 -0500

    Added HTML tags.

--- codeparser.php
+++ codeparser.php
@@ -7,8 +7,9 @@ if (strtoupper($code) == "BTC NOW") {
   $decode = json_decode($response, true);
 
   $price = $decode['USD'][]
+  echo "<html><head><title>BTC NOW</title></head><body>";
   echo "<h1>" . $code . "</h1></br>";
   echo "<h2>Bid: " . $decode['USD']['buy'] . "</h2></br>";
-  echo "<h2>Ask: " . $decode['USD']['sell'] . "</h2></br>";
+  echo "<h2>Ask: " . $decode['USD']['sell'] . "</h2></br></body></html>";
 }
 ?>
